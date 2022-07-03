958bc74e0b16e7780af196725692897125d4218d
AnjuPaul14 <101974961+AnjuPaul14@users.noreply.github.com>
2022-07-03 07:33:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def predict():
     return render_template('index.html', prediction_text='The category of the flower is  {}'.format(o1))
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=False)
